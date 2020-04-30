ce138cca7d322eef5243b0e990b91243d152182b
sangeet <sangeet.khatri@protonmail.com>
2020-05-01 02:26:02 +0530

    Made work page responsive

--- src/pages/work/index.js
+++ src/pages/work/index.js
@@ -16,16 +16,18 @@ const WorkItem = () => {
       <div
         className="h-full w-full absolute inset-0 z-10 flex flex-col justify-center items-center text-center font-bold"
       >
-        <h1 className="text-6xl">Stanley Editor</h1>
-        <span className="text-2xl mt-3 mb-10 w-1/3"
-          >Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae et
-          iure ab ea, fugiat minus?</span
-        >
-        <a
-          href="/"
-          className="bg-blue-700 text-white px-10 py-4 font-bold uppercase"
-          >Enter</a
-        >
+        <div className="flex flex-col items-center justify-center px-16 w-full md:w-10/12 lg:w-8/12 xl:w-7/12">
+          <h1 className="text-4xl md:text-5xl lg:text-6xl">Stanley Editor</h1>
+          <span className="text-lg md:text-2xl mt-3 mb-10"
+            >Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+            et iure ab ea, fugiat minus?</span
+          >
+          <a
+            href="/"
+            className="bg-blue-700 text-white px-10 py-4 font-bold uppercase"
+            >Enter</a
+          >
+        </div>
       </div>
       <img
         src="https://via.placeholder.com/1920x1080/EEEEEE/CCCCCC"
