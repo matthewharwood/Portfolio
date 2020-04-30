05f641f52f9cb228b40061d79c9c95e20b8c5a75
Matthew Harwood <matthhar12@gmail.com>
2020-04-29 23:05:18 -0700

    Added boilerplate for homepage

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -6,7 +6,7 @@ import {navigationLinks} from '../../_data/navigation';
 const Navigation = () => {
   return html`
     <nav class="fixed bottom-0 right-0 items-end md:grid md:row-start-2 md:row-end-3 md:col-start-2 md:col-end-5 lg:col-start-4 items-end pr-4 pb-2 md:pb-16 md:pr-16 box-border">
-        ${navigationLinks.map((nl, index) => html`<${AH.NavigationItem} link="${nl.link}" index="${index}"/>`)}
+      ${navigationLinks.map((nl, index) => html`<${AH.NavigationItem} link="${nl.link}" index="${index}"/>`)}
     </nav>
   `
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,13 +4,15 @@ import {Navigation} from '../components/navigation';
 
 export const Index = () => {
   return html`
-
-    <div data-router-view="index">
-      <${Navigation} />
-      <div >
-       <${AH.RunningHeader} />
+    <div class="md:grid md:grid-rows-2 md:grid-cols-4 md:gap-4 h-screen bg-gray-200" data-router-view="index">
+      <img class="md:grid col-span-4 pt-8 pl-8 pr-32 md:pt-16 md:pl-16 md:pr-0 box-border" src="http://localhost:3000/static/good_morning_harwood_logo.svg" alt="Good Morning Harwood Logo" />
+      <div class=" md:grid md:row-end-3 md:col-span-2 pt-4 pl-8 md:pb-16 md:pl-16 items-end box-border">
+        <${AH.RunningHeader} />
       </div>
+      <div>
+        <div class="fixed inset-0"></div>
+      </div>
+      <${Navigation} />
     </div>
-   
   `;
 };
