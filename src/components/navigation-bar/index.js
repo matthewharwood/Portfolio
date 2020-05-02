4443bcd56a6d2948969192f4a0ef2bf963201f92
Matthew Harwood <matthhar12@gmail.com>
2020-05-01 23:38:14 -0700

    Added active class to navigation

--- src/components/navigation-bar/index.js
+++ src/components/navigation-bar/index.js
@@ -1,51 +1,63 @@
-import { html } from "htm/preact";
-import {useRef, useLayoutEffect} from 'preact/hooks';
+import {html} from 'htm/preact';
+import {useLayoutEffect, useRef, useState} from 'preact/hooks';
+import {isServer} from '../../utils/which_env';
 
+const calcActiveLink = (s, pathname) => s.map(i => {
+  if(isServer) return;
+  if (i.href === pathname) {
+    i.isActive = true;
+  } else {
+    i.isActive = false;
+  }
+  return i;
+});
 
 export const NavigationBar = () => {
+
   const links = [
     {
-      title: "Work",
-      href: "/work",
+      title: 'Work',
+      href: '/work',
     },
     {
-      title: "Lab",
-      href: "/lab",
+      title: 'Lab',
+      href: '/lab',
     },
     {
-      title: "Profile",
-      href: "/profile",
+      title: 'Profile',
+      href: '/profile',
     },
   ];
-  const navRef = useRef(null);
-  useLayoutEffect(() => {
-    navRef.current.classList.add("transition", "duration-150", "ease-in-out");
-    const hide = () => {
-      navRef.current.classList.add("hide-nav");
-      navRef.current.classList.remove("show-nav");
-    }
-    const show = () =>  {
-      navRef.current.classList.add("show-nav");
-      navRef.current.classList.remove("hide-nav");
-    }
 
-    if(location.pathname !== '/') {
+
+  const navRef = useRef(null);
+  const [stateLink, updateStateLinks] = useState(links);
+  useLayoutEffect(() => {
+    navRef.current.classList.add('transition', 'duration-150', 'ease-in-out');
+    const hide = () => {
+      navRef.current.classList.add('hide-nav');
+      navRef.current.classList.remove('show-nav');
+    };
+    const show = () => {
+      navRef.current.classList.add('show-nav');
+      navRef.current.classList.remove('hide-nav');
+    };
+    calcActiveLink(links);
+    if (location.pathname !== '/') {
       show();
     }
+    updateStateLinks(calcActiveLink(links, location.pathname));
     import('../../router').then(({InitRouter}) => {
-      console.log(InitRouter.getRouter());
-      InitRouter.getRouter().on('NAVIGATE_IN', ({ to, trigger, location }) => {
-        console.log('is this working');
-        if(location.pathname === '/') {
-          console.log('is this working /', navRef.current);
+      InitRouter.getRouter().on('NAVIGATE_IN', ({to, trigger, location}) => {
+        updateStateLinks(calcActiveLink(links, location.pathname));
+        if (location.pathname === '/') {
           hide();
         } else {
-          console.log('is this not working /', navRef.current);
           show();
         }
       });
-    })
-  })
+    });
+  }, []);
 
   return html`
     <header ref="${navRef}" className="nav-styles hide-nav">
@@ -57,24 +69,20 @@ export const NavigationBar = () => {
           </span>
         </a>
         <ul className="flex flex-col md:flex-row items-start md:items-center">
-          <${NavLink}
-            data="${{ title: "Active", href: "test" }}"
-            isActive="${true}"
-          />
-          ${links.map(
-            (link) => html` <${NavLink} data="${link}" isActive="${false}" /> `
-          )}
+          ${stateLink.map(
+      (link) => html` <${NavLink} ...${link} /> `
+  )}
         </ul>
       </div>
     </header>
   `;
 };
 
-const NavLink = ({ data, isActive }) => {
-  const activeClass = isActive ? "font-bold" : "";
+const NavLink = ({title, href, isActive}) => {
+  const activeClass = isActive ? 'bg-gray-900' : '';
   return html`
-    <li className="mt-2 py-1 md:my-0 md:py-0 md:ml-4 ${activeClass}">
-      <a href="${data.href}" className="md:px-3 py-2 rounded-full bg-transparent hover:bg-gray-800">${data.title}</a>
+    <li className="mt-2 py-1 md:my-0 md:py-0 md:ml-4 ">
+      <a href="${href}" className="md:px-3 py-2 rounded-full bg-transparent hover:bg-gray-800 ${activeClass}">${title}</a>
     </li>
   `;
 };
