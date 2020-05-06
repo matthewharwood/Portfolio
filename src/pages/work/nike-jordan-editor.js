916eb974492ec7699383b5a9c29a90d0384ca0b2
sangeet <sangeet.khatri@protonmail.com>
2020-05-07 03:33:44 +0530

    Added data to nike-jordan-editor and added more parameters accordingly

--- src/pages/work/nike-jordan-editor.js
+++ src/pages/work/nike-jordan-editor.js
@@ -1,170 +1,47 @@
 import { html } from "htm/preact";
 import { AH } from "../../components/component_map";
+import { data } from './nike-jordan-data'
 
-const BG_COLOR = "bg-gray-100"
+const BG_COLOR = "bg-gray-100";
 
 export const WorkNikeJordanEditor = () => {
-  const postHeaderBlockData = {
-    heading: "Nike Jordan Editor",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas id laudantium explicabo repellendus magnam laborum quae totam ut debitis. At?",
-    subtext: "Web Application",
-    info: [
-      {
-        heading: "Client",
-        text: "Nike Jordan",
-      },
-      {
-        heading: "Client",
-        text: "Nike Jordan",
-      },
-      {
-        heading: "Client",
-        text: "Nike Jordan",
-      },
-      {
-        heading: "Client",
-        text: "Nike Jordan",
-      },
-      {
-        heading: "Client",
-        text: "Nike Jordan",
-      },
-      {
-        heading: "Client",
-        text: "Nike Jordan",
-      },
-    ],
-  };
-  const summaryBlockData = {
-    number: "00",
-    heading: "Summary",
-    text:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui blanditiis autem voluptates, delectus earum molestias laborum repellat tenetur sapiente quas culpa accusantium suscipit esse in. Dolor eveniet laboriosam, atque ex nam excepturi ea cumque neque. Natus molestiae sequi dolores! Nesciunt excepturi voluptatem recusandae esse labore quisquam officia earum voluptas laudantium laboriosam hic atque veniam nobis doloribus repellat cupiditate, iusto facere saepe molestiae. Temporibus accusantium, quam autem suscipit cumque fugit molestias rerum? Ut, magni nostrum? Quis eos iusto dicta? Quibusdam, accusantium laboriosam asperiores beatae ipsam, nisi, mollitia voluptatum ratione assumenda id placeat omnis cum? Soluta amet alias commodi corporis molestiae assumenda?",
-    hasBackground: false,
-  };
-  const outlineBlockData = {
-    title: "process",
-    steps: ["one", "two", "three", "four", "five"],
-    hasBackground: false,
-  };
-  const highlightBlockData = {
-    subtitle: "00",
-    title: "Getting started",
-    text: "lorem ipsum dolor sit amet",
-    image: "https://via.placeholder.com/800x600",
-  };
-  const highlightBoldBlockData = {
-    subtitle: "00",
-    title: "Getting started",
-    text: "lorem ipsum dolor sit amet",
-    image: "https://via.placeholder.com/800x600",
-    hasBackground: true,
-  };
-  const quoteBlockData = {
-    quote:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Error accusantium nam eaque natus beatae aperiam blanditiis libero. Ducimus, ex tenetur?",
-    person: "Jimmy Soat",
-    title: "Design Director",
-    date: "February 2014",
-    hasBackground: true,
-  };
-  const headlineBlockData = {
-    title: "Challenge",
-    text:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo cupiditate nam possimus odit rerum aliquid nisi delectus culpa sint pariatur?",
-    hasBackground: true,
-  };
-  const cardTileBlockData = {
-    cards: [
-      {
-        title: "Color Palette",
-        subtitle: "Two Tone",
-        image: "https://via.placeholder.com/300",
-      },
-      {
-        title: "Another Title",
-        subtitle: "Two Tone",
-        image: "https://via.placeholder.com/300",
-      },
-    ],
-    hasBackground: true,
-  };
-  const videoBlockData = {
-    videoSrc: "https://www.w3schools.com/html/mov_bbb.mp4",
-    title: "Making a Project",
-    text:
-      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit, pariatur. Ipsam veritatis inventore pariatur nulla nihil rem temporibus. Sed, consequatur?",
-    hasBackground: true,
-  };
-  const stepCardsBlockData = {
-    steps: [
-      {
-        title: "Heading",
-        subtitle: "lorem ipsum",
-        image: "https://via.placeholder.com/100",
-      },
-      {
-        title: "Heading",
-        subtitle: "lorem ipsum",
-        image: "https://via.placeholder.com/100",
-      },
-      {
-        title: "Heading",
-        subtitle: "lorem ipsum",
-        image: "https://via.placeholder.com/100",
-      },
-      {
-        title: "Heading",
-        subtitle: "lorem ipsum",
-        image: "https://via.placeholder.com/100",
-      },
-      {
-        title: "Heading",
-        subtitle: "lorem ipsum",
-        image: "https://via.placeholder.com/100",
-      },
-      {
-        title: "Heading",
-        subtitle: "lorem ipsum",
-        image: "https://via.placeholder.com/100",
-      },
-    ],
-    hasBackground: true,
-  };
-  const cardTilesTwoData = {
-    cards: [
-      {
-        title: "Color Palette",
-        subtitle: "Two Tone",
-        image: "https://via.placeholder.com/300",
-        text:
-          "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia consectetur, corporis quos accusantium nesciunt iste, similique deserunt amet ipsam maiores debitis beatae sapiente tenetur autem earum culpa nobis iusto a!",
-      },
-      {
-        title: "Another Title",
-        subtitle: "Two Tone",
-        image: "https://via.placeholder.com/300",
-        text:
-          "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia consectetur, corporis quos accusantium nesciunt iste, similique deserunt amet ipsam maiores debitis beatae sapiente tenetur autem earum culpa nobis iusto a!",
-      },
-    ],
-    hasBackground: true,
-  };
   return html`
     <div data-router-view="work">
-      <${PostHeaderBlock} ...${postHeaderBlockData} />
-      <${SummaryBlock} ...${summaryBlockData} />
-      <${OutlineBlock} ...${outlineBlockData} />
-      <${HighlightBlock} ...${highlightBlockData} />
-      <${HighlightBoldBlock} ...${highlightBoldBlockData} />
-      <${QuoteBlock} ...${quoteBlockData} />
-      <${HeadlineBlock} ...${headlineBlockData} />
-      <${HeadlineBlock} ...${headlineBlockData} />
-      <${CardTileBlock} ...${cardTileBlockData} />
-      <${VideoBlock} ...${videoBlockData} />
-      <${StepsCardsBlock} ...${stepCardsBlockData} />
-      <${CardTilesBlock} ...${cardTilesTwoData} />
+      <${PostHeaderBlock} ...${data.postHeaderBlockData} />
+      <${SummaryBlock} ...${data.summaryBlockData} />
+      <${OutlineBlock} ...${data.outlineBlockData} />
+      <${HighlightBlock} ...${data.highlightBlockDataOne} />
+      <${HighlightBlock} ...${data.highlightBlockDataTwo} />
+      <${HighlightBoldBlock} ...${data.highlightBoldBlockData} />
+      <${SummaryBlock} ...${data.summaryBlockDataTwo} />
+      <${QuoteBlock} ...${data.quoteBlockData} />
+      <${HeadlineBlock} ...${data.headlineBlockData} />
+      <${HeadlineBlock} ...${data.headlineBlockDataTwo} />
+      <${CardTileBlock} ...${data.cardTileBlockData} />
+      <${HeadlineBlock} ...${data.headlineBlockDataThree} />
+      <${VideoBlock} ...${data.videoBlockData} />
+      <${VideoBlock} ...${data.videoBlockDataTwo} />
+      <${VideoBlock} ...${data.videoBlockDataThree} />
+      <${VideoBlock} ...${data.videoBlockDataFour} />
+      <${VideoBlock} ...${data.videoBlockDataFive} />
+      <${SummaryBlock} ...${data.summaryBlockDataThree} />
+      <${QuoteBlock} ...${data.quoteBlockDataTwo} />
+      <${HeadlineBlock} ...${data.headlineBlockDataFour} />
+      <${HeadlineBlock} ...${data.headlineBlockDataFive} />
+      <${StepsCardsBlock} ...${data.stepCardsBlockData} />
+      <${HeadlineBlock} ...${data.headlineBlockDataSix} />
+      <${HeadlineBlock} ...${data.headlineBlockDataSeven} />
+      <${HeadlineBlock} ...${data.headlineBlockDataEight} />
+      <${CardTilesBlock} ...${data.cardTilesTwoData} />
+      <${HeadlineBlock} ...${data.headlineBlockDataNine} />
+      <${HeadlineBlock} ...${data.headlineBlockDataTen} />
+      <${HeadlineBlock} ...${data.headlineBlockDataEleven} />
+      <${CardTilesBlock} ...${data.cardTilesTwoDataNodeWebkit} />
+      <${HeadlineBlock} ...${data.headlineBlockDataTwelve} />
+      <${HighlightBlock} ...${data.highlightBlockDataThree} />
+      <${HighlightBlock} ...${data.highlightBlockDataFour} />
+      <${HighlightBoldBlock} ...${data.highlightBoldBlockDataTwo} />
+      <${HighlightBlock} ...${data.highlightBlockDataFive} />
     </div>
   `;
 };
@@ -181,7 +58,7 @@ const PostHeaderBlock = ({ heading, description, subtext, info }) => {
           <p className="text-xl">${description}</p>
         </div>
         <div
-          className="grid grid-cols-1 lg:grid-cols-3 border-t border-gray-700 mt-5 pt-5"
+          className="grid grid-flow-col grid-cols-1 lg:grid-cols-3 grid-rows-2 border-t border-gray-700 mt-5 pt-5"
         >
           ${info.map(
             (infoItem) => html` <${PostHeaderColumn} ...${infoItem} /> `
@@ -203,26 +80,41 @@ const PostHeaderColumn = ({ heading, text }) => {
   `;
 };
 
-const SummaryBlock = ({ hasBackground, number, heading, text }) => {
+const SummaryBlock = ({
+  hasBackground = false,
+  number,
+  heading,
+  text,
+  hasBorder = false,
+}) => {
   const bgClass = hasBackground ? BG_COLOR : "";
   return html`
-    <section className="border-b border-gray-400 py-16 ${bgClass}">
-      <div className="container px-5 lg:px-16 grid grid-cols-3">
+    <section className="${bgClass} flex flex-col items-center">
+      <div className="container py-16 px-5 lg:px-16 grid grid-cols-3">
         <div className="col-span-3 lg:col-span-2 flex flex-col">
           <span className="text-2xl text-red-500">${number}</span>
           <h2 className="text-5xl">${heading}</h2>
           <p className="mt-5 leading-loose">${text}</p>
         </div>
         <div
-          className="lg:row-start-2 col-span-3 lg:col-span-2 grid grid-cols-1 lg:grid-cols-2 gap-10 mt-16"
+          className="hidden lg:row-start-2 col-span-3 lg:col-span-2 grid grid-cols-1 lg:grid-cols-2 gap-10 mt-16"
         >
           <${VideoCard} />
           <${VideoCard} />
         </div>
       </div>
+      ${hasBorder && html`<${Border}/>` }
     </section>
   `;
 };
+
+const Border = () => {
+  return html`
+        <div className="container px-5 lg:px-16 w-full">
+          <div className="h-px w-full bg-gray-400"></div>
+        </div>
+  `
+}
 
 const VideoCard = () => {
   return html`
@@ -249,27 +141,27 @@ const OutlineBlock = ({ hasBackground, title, steps }) => {
   return html`
     <section className="py-16 ${bgClass}">
       <div className="container px-5 lg:px-16 grid grid-cols-12">
-      <div
-        className="col-span-12 lg:col-span-5 flex items-center lg:block lg:items-start mb-10 lg:mb-0"
-      >
-        <h3 className="text-3xl uppercase flex-shrink-0">${title}</h3>
         <div
-          className="ml-5 w-full border-t border-b border-gray-600 lg:hidden"
-        ></div>
-      </div>
+          className="col-span-12 lg:col-span-5 flex items-center lg:block lg:items-start mb-10 lg:mb-0"
+        >
+          <h3 className="text-3xl uppercase flex-shrink-0">${title}</h3>
+          <div
+            className="ml-5 w-full border-t border-b border-gray-600 lg:hidden"
+          ></div>
+        </div>
         <div className="col-span-12 lg:col-span-7">
           <ul>
-    ${steps.map(
-      (step, index) => html`
-        <${OutlineListItem}
-          text="${step}"
-            isLast="${index === steps.length - 1}"
-      />
-      `
-          )}
-        </ul>
-          </div>
-    </div>
+            ${steps.map(
+              (step, index) => html`
+                <${OutlineListItem}
+                  text="${step}"
+                  isLast="${index === steps.length - 1}"
+                />
+              `
+            )}
+          </ul>
+        </div>
+      </div>
     </section>
   `;
 };
@@ -293,35 +185,55 @@ const OutlineListItem = ({ text, isLast = false }) => {
   `;
 };
 
-const HighlightBlock = ({ subtitle, title, text, image }) => {
+const HighlightBlock = ({
+  isImageRight,
+  imageCols = 6,
+  subtitle,
+  title,
+  text,
+  image,
+}) => {
+  const imageOrder = isImageRight ? "order-last" : "order-first";
+  const imageColsClass = `lg:col-span-${imageCols}`;
+  const textColsClass = `lg:col-span-${12 - imageCols}`;
   return html`
     <section
       className="container px-5 lg:px-16 grid grid-cols-12 lg:gap-10 py-16"
     >
-      <div className="flex flex-col col-span-12 lg:col-span-6">
+      <div className="flex flex-col col-span-12 ${textColsClass}">
         <span className="text-2xl text-red-500">${subtitle}</span>
         <h2 className="text-5xl">${title}</h2>
         <p className="mt-5 leading-loose">${text}</p>
       </div>
-      <div className="col-span-12 lg:col-span-6">
+      <div className="col-span-12 ${imageOrder} ${imageColsClass}">
         <img src="${image}" alt="" className="h-full w-full object-cover" />
       </div>
     </section>
   `;
 };
 
-const HighlightBoldBlock = ({ subtitle, title, text, image }) => {
+const HighlightBoldBlock = ({
+  isImageRight,
+  imageCols = 6,
+  subtitle,
+  title,
+  text,
+  image,
+}) => {
+  const imageOrder = isImageRight ? "order-last" : "order-first";
+  const imageColsClass = `lg:col-span-${imageCols}`;
+  const textColsClass = `lg:col-span-${12 - imageCols}`;
   return html`
     <section className="bg-white py-16 px-5 lg:px-0">
       <div
         className="container px-5 lg:px-16 grid grid-cols-12 lg:gap-10 py-16 bg-gray-100"
       >
-        <div className="flex flex-col col-span-12 lg:col-span-6">
+        <div className="flex flex-col col-span-12 ${textColsClass}">
           <span className="text-2xl text-red-500">${subtitle}</span>
           <h2 className="text-5xl">${title}</h2>
           <p className="mt-5 leading-loose">${text}</p>
         </div>
-        <div className="col-span-12 lg:col-span-6">
+        <div className="col-span-12 ${imageOrder} ${imageColsClass}">
           ${image &&
           html`
             <img
@@ -336,13 +248,13 @@ const HighlightBoldBlock = ({ subtitle, title, text, image }) => {
   `;
 };
 
-const QuoteBlock = ({ hasBackground, quote, person, title, date }) => {
+const QuoteBlock = ({ hasBackground, hasBorder=false, quote, person, title, date }) => {
   const bgClass = hasBackground ? BG_COLOR : "";
   return html`
-    <section className="${bgClass} py-16 lg:py-32">
-      <div className="container px-5 lg:px-16 flex flex-col items-center">
+    <section className="${bgClass}">
+      <div className="container px-5 lg:px-16 py-16 lg:py-32 flex flex-col items-center">
         <div className="w-full lg:w-1/2 flex flex-col">
-          <p className="relative italic text-lg">"${quote}"</p>
+          <p className="relative italic text-lg">“${quote}”</p>
           <div
             className="flex flex-col self-end text-xs justify-end text-right text-gray-600 mt-5"
           >
@@ -352,20 +264,22 @@ const QuoteBlock = ({ hasBackground, quote, person, title, date }) => {
           </div>
         </div>
       </div>
+      ${hasBorder && html`<${Border}/>` }
     </section>
   `;
 };
 
-const HeadlineBlock = ({ hasBackground, title, text }) => {
+const HeadlineBlock = ({ hasBackground, hasBorder=false, title, text }) => {
   const bgClass = hasBackground ? BG_COLOR : "";
   return html`
-    <section className="${bgClass} py-8 lg:py-16">
-      <div className="container px-5 lg:px-16 grid grid-cols-3">
+    <section className="${bgClass}">
+      <div className="container px-5 lg:px-16 py-8 lg:py-16 grid grid-cols-3">
         <div className="col-span-3 lg:col-span-1">
           <h3 className="text-3xl uppercase">${title}</h3>
         </div>
         <div className="col-span-3 lg:col-span-2">${text}</div>
       </div>
+      ${hasBorder && html`<${Border}/>` }
     </section>
   `;
 };
@@ -393,7 +307,9 @@ const Card = ({ data }) => {
         alt=""
         className="h-64 w-64 lg:mr-10 object-cover"
       />
-      <div className="flex flex-col leading-loose mt-5">
+      <div
+        className="flex flex-col leading-loose mt-5 items-center lg:items-start"
+      >
         <h5 className="uppercase font-bold text-gray-600">${data.title}</h5>
         <span>${data.subtitle}</span>
       </div>
@@ -420,23 +336,61 @@ const VideoBlock = ({ hasBackground, videoSrc, title, text }) => {
 
 const StepsCardsBlock = ({ hasBackground, steps }) => {
   const bgClass = hasBackground ? BG_COLOR : "";
+  const colorsArray = [
+    "teal-400",
+    "red-400",
+    "blue-400",
+    "green-400",
+    "yellow-400",
+    "orange-400",
+  ];
+  const categories = [...new Set(steps.map((step) => step.category))];
+  const categoryData = categories.map((category) => ({
+    name: category,
+    cards: steps.filter((step) => step.category === category),
+  }));
   return html`
     <section className="${bgClass} py-16">
       <div className="container sm:px-5 lg:px-16 grid grid-cols-3">
         <div
           className="lg:col-start-2 col-span-3 lg:col-span-2 grid grid-cols-1 lg:grid-cols-2 gap-10"
         >
-          ${steps.map((step) => html` <${StepCard} data="${step}" /> `)}
+          ${categoryData.map(
+            (category, index) => html`
+              <div className="grid grid-cols-1 gap-5 lg:gap-10">
+                <${StepLabel}
+                  name="${category.name}"
+                  color="${colorsArray[index]}"
+                  mobileOnly="${true}"
+                />
+                ${category.cards.map(
+                  (card) => html`
+                    <${StepCard} data="${card}" color="${colorsArray[index]}" />
+                  `
+                )}
+              </div>
+            `
+          )}
+        </div>
+        <div className="hidden lg:flex col-start-2 col-span-2 justify-end mt-5">
+          ${categories.map(
+            (category, index) =>
+              html`<${StepLabel}
+                name="${category}"
+                color="${colorsArray[index]}"
+              />`
+          )}
         </div>
       </div>
     </section>
   `;
 };
 
-const StepCard = ({ data }) => {
+const StepCard = ({ data, color }) => {
+  const borderColor = `border-${color}`;
   return html`
     <div
-      className="bg-gray-200 grid grid-cols-4 gap-10 p-5 border-t-4 border-red-500 shadow"
+      className="bg-gray-200 grid grid-cols-4 gap-10 p-5 border-t-4 ${borderColor} shadow"
     >
       <div className="col-span-1">
         <img src="${data.image}" alt="" />
@@ -451,10 +405,25 @@ const StepCard = ({ data }) => {
   `;
 };
 
+const StepLabel = ({
+  name = "test",
+  color = "red-400",
+  mobileOnly = false,
+}) => {
+  const backgroundColor = `bg-${color}`;
+  const display = mobileOnly ? "flex lg:hidden" : "flex";
+  return html`
+    <div className="${display} items-center ml-5">
+      <div className="h-5 w-5 ${backgroundColor} mr-3" />
+      <span className="text-gray-600 uppercase text-sm">${name}</span>
+    </div>
+  `;
+};
+
 const CardTilesBlock = ({ hasBackground, cards }) => {
   const bgClass = hasBackground ? BG_COLOR : "";
   return html`
-    <section className="${bgClass} py-16">
+    <section className="${bgClass} py-5">
       <div className="container sm:px-5 lg:px-16 grid grid-cols-3">
         <div
           className="lg:col-start-2 col-span-3 lg:col-span-2 grid grid-cols-1 lg:grid-cols-2 gap-10"
@@ -468,9 +437,7 @@ const CardTilesBlock = ({ hasBackground, cards }) => {
 
 const CardTile = ({ data }) => {
   return html`
-    <div
-      className="bg-gray-200 grid grid-cols-4 gap-10 p-5 border-t-4 border-red-500 shadow"
-    >
+    <div className="bg-gray-200 grid grid-cols-4 gap-10 p-5 shadow">
       <div className="col-span-1">
         <img src="${data.image}" alt="" />
       </div>
