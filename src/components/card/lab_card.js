a5910643e54095a5999812f4703d988ae04f8084
sangeet <sangeet.khatri@protonmail.com>
2020-05-07 23:44:36 +0530

    Added lab layout

--- src/components/card/lab_card.js
+++ src/components/card/lab_card.js
@@ -2,25 +2,31 @@ import {useStatic} from '../../hooks/use_static';
 import {html} from 'htm/preact';
 
 
-const LabCard = ({link}) => {
-  const src = useStatic('sample.jpg');
+const LabCard = ({ link }) => {
+  const src = useStatic("sample.jpg");
   return html`
-  <div class="rounded overflow-hidden shadow-lg">
-    <img class="w-full" src="${src}" alt="Sunset in the mountains" />
-    <div class="px-6 py-4">
-      <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
-      <p class="text-gray-700 text-base">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-      </p>
+    <div className="rounded overflow-hidden shadow-lg m-10">
+      <img className="w-full" src="${src}" alt="Sunset in the mountains" />
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+        <p className="text-gray-700 text-base">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
+          quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
+          nihil.
+        </p>
+      </div>
+      <div className="px-6 py-4">
+        ${["photography", "travel", "winter"].map(
+          (tag) => html`
+            <span
+              className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+              >${tag}</span>
+          `
+        )}
+      </div>
     </div>
-    <div class="px-6 py-4">
-      <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#photography</span>
-      <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#travel</span>
-      <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#winter</span>
-    </div>
-  </div>
-  `
-}
+  `;
+};
 
 export {
   LabCard,
--- src/pages/lab/index.js
+++ src/pages/lab/index.js
@@ -1,20 +1,57 @@
 import {LabCard} from '../../components/card/lab_card';
 import { AH } from "../../components/component_map";
-import {html} from 'htm/preact';
+import { useStatic } from "../../hooks/use_static";
+import { html } from "htm/preact";
+const PADDING = "p-10";
 
 export const Lab = () => {
   return html`
-    <div class="flex flex-wrap m-auto p-2 px-4 lg:px-16 4bg-gray-200 min-h-screen relative" data-router-view="lab">
-      <div class="w-full">
-        <h1 class="text-6xl">Lab</h1>
+    <div className="pt-32 container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3" data-router-view="lab">
+      <div className="col-span-1 leading-tight border ${PADDING}">
+        <h1 className="text-6xl font-bold">Lab</h1>
+        <span className="text-xl font-bold">A Laboratory of Experiments</span>
       </div>
-      ${[1,2,3,4,5,6].map(() => {
-        return html`
-            <a class="px-3 w-full w-full mb-4 lg:w-1/3" href="${'http://localhost:8000/lab/rorschach'}">
-                <${LabCard} />
-            </a>
-        `    
-      })}  
+      <div className="border-l border-r px-2 hidden lg:block">
+        <div className="w-full h-full border"></div>
+      </div>
+      <div className="border"></div>
+      <${Row} hasBorderLeft="${false}" hasBorderRight="${false}" />
+      <${Row} hasBorderLeft="${true}" hasBorderRight="${true}" />
+      <${Row} />
     </div>
   `;
 };
+
+const HeadingBlock = ({ hasBorderLeft = false, hasBorderRight = false }) => {
+  const borderLeft = hasBorderLeft ? "lg:ml-2 lg:border-l" : "";
+  const borderRight = hasBorderRight ? "lg:mr-2 lg:border-r" : "";
+  return html`
+    <div className="border-t border-b ${borderLeft} ${borderRight} ${PADDING}">
+      <h2 className="text-3xl font-bold">Todo</h2>
+    </div>
+  `;
+};
+
+const Row = ({ hasBorderLeft = false, hasBorderRight = false }) => {
+  const borderLeft = hasBorderLeft ? "lg:ml-2 lg:border-l" : "";
+  const borderRight = hasBorderRight ? "lg:mr-2 lg:border-r" : "";
+  return html`
+    <div className="min-h-screen border py-2 flex flex-col mb-10">
+      <${HeadingBlock}
+        hasBorderLeft="${hasBorderLeft}"
+        hasBorderRight="${hasBorderRight}"
+      />
+      <div className="border-t ${borderLeft} ${borderRight} mt-2">
+        ${[1, 2].map(
+          (card) => html`
+            <div className="lg:mb-10 border-b">
+              <${LabCard} />
+              <div className="lg:border-b mb-2" />
+            </div>
+          `
+        )}
+      </div>
+    </div>
+  `;
+};
+
