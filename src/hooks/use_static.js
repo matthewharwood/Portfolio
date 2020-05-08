e725b10ca9864bc1427c4b2355f64d7f1fc39a3e
Matthew Harwood <matthhar12@gmail.com>
2020-05-07 21:08:58 -0700

    update package.json

--- src/hooks/use_static.js
+++ src/hooks/use_static.js
@@ -1,6 +1,5 @@
 export const useStatic = (path, ...flags) => {
   if (!path) return
-  console.log(path)
   let base = `https://res.cloudinary.com/morningharwood/image/upload/${['f_auto','q_auto', ...flags].join(',')}/`;
   if(path.includes('.mp4')) {
     base =  `https://res.cloudinary.com/morningharwood/video/upload/${[...flags].join(',')}/`;
