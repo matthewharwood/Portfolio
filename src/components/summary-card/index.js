fc7b90c82b885ee289b2d7e0839836206433bd5b
sangeet <sangeet.khatri@protonmail.com>
2020-05-10 06:46:12 +0530

    Added params to sumary-card

--- src/components/summary-card/index.js
+++ src/components/summary-card/index.js
@@ -1,20 +1,22 @@
 import { html } from "htm/preact";
 
-export const SummaryCard = () => {
+export const SummaryCard = ({ heading="Fusion.js", list=["one", "two", "three"] }) => {
   return html`
-    <div className="border border-primary p-4 flex flex-col text-sm">
-      <h6 className="mt-2 mb-4 text-lg font-mono font-bold">Fusion.js</h6>
-      <ul>
-        ${[1, 2, 3, 4].map(
-          (point) => html`
-            <li className="flex items-center mb-2">
-              <img src="https://via.placeholder.com/50" alt="" className="h-4 w-4"/>
-              <span className="ml-2">Isomorphic Framework</span>
-            </li>
-          `
+    <div style="padding-bottom:100%" className="relative">
+      <div className="absolute inset-0 border border-primary p-4 flex flex-col text-sm">
+      <h6 className="mt-2 mb-4 text-lg font-mono font-bold">${heading}</h6>
+        <ul>
+    ${list.map(
+      (listItem) => html`
+          <li className="flex items-center mb-2">
+          <img src="https://via.placeholder.com/50" alt="" className="h-4 w-4"/>
+          <span className="ml-2">${listItem}</span>
+      </li>
+      `
         )}
-      </ul>
-        <a href="#" className="mt-4 bg-primary text-secondary font-mono w-full py-2 text-center">Learn More</a>
+          </ul>
+      <a href="#" className="mt-auto bg-primary text-secondary font-mono w-full py-2 text-center" aria-label="${heading}">Learn More</a>
+    </div>
     </div>
   `;
 };
