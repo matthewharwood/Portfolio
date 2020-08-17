d002ada8afdfbfd569163b3df41bde9c149bb324
Matthew Harwood <matthhar12@gmail.com>
2020-08-16 19:33:56 -0700

    added script tag

--- src/pages/document.js
+++ src/pages/document.js
@@ -41,6 +41,11 @@ export const HtmlPage = ({ title, content, navigation, polymorph }) => {
           });
         }
         </script>
+        <script>
+          window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
+          ga('create', 'UA-31350832-2', 'auto'); ga('set', 'anonymizeIp', true); ga('set', 'transport', 'beacon'); ga('send', 'pageview')
+        </script>
+        <script src="https://www.google-analytics.com/analytics.js" async></script>
       </body>
     </html>
   `;
