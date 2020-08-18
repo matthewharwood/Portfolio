7b0f8e804582f516b8e0000f9ea1089d922e722b
Matthew Harwood <matthhar12@gmail.com>
2020-08-17 22:33:52 -0700

    wip

--- src/components/highlight-vertical/index.js
+++ src/components/highlight-vertical/index.js
@@ -7,7 +7,7 @@ export const HighlightVertical = ({
   title,
   image,
 }) => {
-  const imageOrder = isImageRight ? "order-last" : "order-first";
+  const imageOrder = isImageRight ? "lg:order-last" : "lg:order-first";
   const imageColsClass = `lg:col-span-${imageCols}`;
   const textColsClass = `lg:col-span-${12 - imageCols}`;
   return html`
@@ -15,7 +15,7 @@ export const HighlightVertical = ({
       className="container px-5 lg:px-16 grid grid-cols-12 lg:gap-10 py-16"
     >
       <div className="flex flex-col col-span-12 ${textColsClass}">
-        <h2 className="text-huge transform whitespace-no-wrap rotate-90">${title}</h2>
+        <h2 className="text-4xl lg:text-huge transform whitespace-no-wrap lg:rotate-90">${title}</h2>
       </div>
       <div className="col-span-12 ${imageOrder} ${imageColsClass}">
         <${Image} src="${image}" alt="" className="h-full w-full object-cover" />
--- src/components/team-bubbles/index.js
+++ src/components/team-bubbles/index.js
@@ -4,26 +4,23 @@ import {Image} from '../media';
 const Teammate = (props) =>{
   console.log('innerprops', props)
   return html`
-     <div className="h-0 pt-1/1 relative">
       ${props.name && props.title && html`
+        <div className="h-0 pt-1/1 relative">
         <div className="inset-0 absolute w-full h-full rounded-full overflow-hidden">
           <${Image} src="${props.image}" flags="e_grayscale" className="object-cover h-full min-w-full" />
-        </div>
-      `}
-       ${props.name && props.title && html`
-         <div className="absolute bottom-1/10 left-1/2 flex flex-col items-start">
+        </div><div className="absolute bottom-1/10 left-1/2 flex flex-col items-start">
             <span className="font-mono text-sm bg-white text-black px-2 py-0 mb-1 whitespace-no-wrap shadow">${props.name}</span>
             <span className="font-mono text-sm bg-white text-black px-2 py-0 whitespace-no-wrap shadow">${props.title}</span>
          </div>
-       `}
-    </div>
+         </div>
+      `}
   `
 }
 
 const TeamBubbles = (props) => {
   return html`
     <section className="">
-      <div className="container grid grid-cols-4 gap-20  px-5 lg:px-16 py-8 lg:py-16 ">
+      <div className="container grid grid-cols-1 lg:grid-cols-4 gap-20  px-5 lg:px-16 py-8 lg:py-16 ">
         ${props.teammates.map(i => html`<${Teammate} ...${i} />`)}
       </div>
     </section>
