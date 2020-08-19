7b5629b8f3dd0218e83bfd0f24b0795abc1fa8d5
sangeet <sangeet.khatri@protonmail.com>
2020-08-19 21:19:03 +0530

    Modifications to team-bubbles for mobile

--- src/components/team-bubbles/index.js
+++ src/components/team-bubbles/index.js
@@ -2,29 +2,44 @@ import {html} from 'htm/preact';
 import {Image} from '../media';
 
 const Teammate = (props) =>{
-  console.log('innerprops', props)
   return html`
       ${props.name && props.title && html`
-        <div className="h-0 pt-1/1 relative">
-        <div className="inset-0 absolute w-full h-full rounded-full overflow-hidden">
-          <${Image} src="${props.image}" flags="e_grayscale" className="object-cover h-full min-w-full" />
-        </div><div className="absolute bottom-1/10 left-1/2 flex flex-col items-start">
-            <span className="font-mono text-sm bg-white text-black px-2 py-0 mb-1 whitespace-no-wrap shadow">${props.name}</span>
-            <span className="font-mono text-sm bg-white text-black px-2 py-0 whitespace-no-wrap shadow">${props.title}</span>
-         </div>
-         </div>
+        <div class="w-48 sm:w-64 flex-shrink-0 relative lg:row-start-${props.rowIndex+1} mx-5">
+          <div className="h-0 pt-1/1 relative">
+            <div className="inset-0 absolute w-full h-full rounded-full overflow-hidden">
+              <${Image} src="${props.image}" flags="e_grayscale" className="object-cover h-full min-w-full" />
+            </div>
+          </div>
+          <div className="lg:absolute bottom-1/10 left-1/2 flex flex-col items-start mt-8 lg:mt-0">
+            <span className="font-mono text-sm lg:bg-white text-black px-2 py-0 mb-1 whitespace-no-wrap lg:shadow">${props.name}</span>
+            <span className="font-mono text-sm lg:bg-white text-black px-2 py-0 whitespace-no-wrap lg:shadow">${props.title}</span>
+          </div>
+        </div>
       `}
   `
 }
 
 const TeamBubbles = (props) => {
+  const rows = arrangeTeamIntoRows(props.teammates.slice(0));
   return html`
-    <section className="">
-      <div className="container grid grid-cols-1 lg:grid-cols-4 gap-20  px-5 lg:px-16 py-8 lg:py-16 ">
-        ${props.teammates.map(i => html`<${Teammate} ...${i} />`)}
+    <section>
+      <div className="container lg:px-16 py-8 lg:py-16 flex lg:grid lg:grid-cols-${rows.length} lg:gap-24 overflow-x-auto">
+        ${rows.map((row, rowIndex) => 
+          row.map(teammate => html`
+            <${ Teammate } ...${teammate} rowIndex="${rowIndex}"/>
+          `)
+        )}
       </div>
     </section>
   `;
+}
+
+const arrangeTeamIntoRows = (teammates) => {
+  const rows = [];
+  for(let i = 1; teammates.length > 0 ; i++){
+    rows.push(teammates.splice(0,i));
+  }
+  return rows;
 }
 
 export {
