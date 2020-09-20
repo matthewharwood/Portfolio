d7ef74dd378b8c3b37a782786b94c22d15a60fba
sangeet <sangeet.khatri@protonmail.com>
2020-09-20 21:00:40 +0530

    Removed junk values and component from test page

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,11 +1,10 @@
 import { AH } from "../components/component_map";
 import { html } from "htm/preact";
-import { MediaSection } from "../components/media-section";
 
 export const Test = () => {
   return html`
     <div className="pt-64 bg-gray-100">
-      <${MediaSection}/>
+      Test
     </div>
   `;
 };
