69330c61ed24f3d22415080b149a5c339ca62220
Matthew Harwood <matthhar12@gmail.com>
2020-09-30 14:24:38 -0700

    Added Sandwhich Layout
    @sangeet The component isn't showing tho could you make it work.

---
+++ src/components/sandwich-layout/index.stories.js
@@ -0,0 +1,18 @@
+import {h} from 'preact';
+import {HighlightVideo} from './';
+import {seed} from '../../_data/seed-data';
+
+export default {
+  title: 'Highlight Video'
+};
+
+const FIXTURE = {
+  videoSrc: 'rorschach/rorschach.mp4',
+  title: 'title',
+  text: 'text text text'
+};
+
+export const Main = () => {
+  const props = seed('highlightVideo');
+  return (<HighlightVideo {...props} />)
+};
