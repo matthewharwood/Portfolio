0c92f8340e3b8eb3d7730c481ec0aaee4c1deab3
Matthew Harwood <matthhar12@gmail.com>
2021-01-24 21:03:29 -0800

    Updated Tailwind to 2.0

--- src/components/outline/outline.js
+++ src/components/outline/outline.js
@@ -6,7 +6,7 @@ const Outline = (props) => {
   const title = props && props.title || 'default title';
   const hasDescriptions = props.hasDescriptions ? ['lg:col-span-4', 'lg:col-span-8'] :  ['lg:col-span-6', 'lg:col-span-6']
   return html`
-    <section className="py-16">
+    <section className="py-44">
       <div className="container px-5 lg:px-16 grid grid-cols-12 gap-6">
         <div className="col-span-12 ${hasDescriptions[0]} flex items-center lg:block lg:items-start mb-10 lg:mb-0">
           <h3 className="text-4xl font-mono flex-shrink-0 leading-none">${title}</h3>
