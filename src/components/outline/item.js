f2270d9ce23b63fc560e9ed8646b2c88353ebe36
Sangeet Khatri <sangeet.khatri@protonmail.com>
2021-11-08 19:51:02 +0530

    Fixed design of bullets in Outline

--- src/components/outline/item.js
+++ src/components/outline/item.js
@@ -1,48 +1,52 @@
-import {html} from 'htm/preact';
+import { html } from "htm/preact";
 
 const OutlineListItemDescription = (props) => {
-  if(!props.description) {
+  if (!props.description) {
     return null;
   }
 
-  return html`
-    <p>
-      ${props.description}
-    </p>
-  `
-}
+  return html` <p>${props.description}</p> `;
+};
 
 const OutlineListItemName = (props) => {
-  if(!props.name) {
+  if (!props.name) {
     return null;
   }
 
   return html`
-    <h4 className="text-primary leading-none text-xl">
+    <h4 className="text-primary leading-none text-xl underline">
       ${props.name}
     </h4>
   `;
-}
-
+};
 
 const OutlineListItemBullet = (props) => {
-  if(!props.number) {
+  if (!props.number) {
     return null;
   }
+  const dotsDisplayClass = props.isLast ? "hidden" : "block";
 
   return html`
-    <span className="absolute capitalize text-primary leading-none text-xl">${props.number}</span>
+    <div className="flex flex-col items-center">
+      <div className="capitalize text-primary leading-none text-xl">
+        ${props.number}
+      </div>
+      <div
+        className="border-l h-16 border-black border-dashed my-4 ${dotsDisplayClass}"
+      ></div>
+    </div>
   `;
 };
 
 const OutlineListItem = (props) => {
-  const name = props &&  props.name || 'hello';
+  const name = (props && props.name) || "hello";
   const description = props && props.description;
-  const number = props && `-` || 1;
+  const number = (props && "•") || 1;
+  const gap = description ? "gap-0" : null;
   return html`
-    <li className="relative">
-      <${OutlineListItemBullet} number=${number} />
-      <div className="grid grid-flow-row items-center  ${description ? "gap-6" : null} pl-16 md-pl-0">
+    <li className="flex items-start">
+      <${OutlineListItemBullet} number=${number} isLast="${props.isLast}" />
+      <div className="grid grid-flow-row items-center  ${gap} pl-16 md-pl-0">
         <${OutlineListItemName} name="${name}" />
         <${OutlineListItemDescription} description="${description}" />
       </div>
@@ -50,6 +54,4 @@ const OutlineListItem = (props) => {
   `;
 };
 
-export {
-  OutlineListItem
-}
+export { OutlineListItem };
